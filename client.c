d6f1908d9cf6db2468556e4449b021b810bcc548
Joaobraz1 <92889377+Joaobraz1@users.noreply.github.com>
2023-02-10 18:03:52 +0000

    Add files via upload

--- client.c
+++ client.c
@@ -1,46 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedro-o <jpedro-o@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 15:46:49 by jpedro-o          #+#    #+#             */
+/*   Updated: 2023/02/10 15:52:01 by jpedro-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mini_talk.h"
 
-int   melt(int pid, char* msg)
+void	chessburger(int bacon)
 {
-   size_t   i;
-   int      j;
-   int      bit;
-
-   i = 0;
-   while (i <= ft_strlen(msg))
-   {
-      j = 8;
-      while (j)
-      {
-         bit = msg[i] >> (--j) & 1;
-         if(bit)
-         {
-            if(kill(pid,SIGUSR1)== -1)
-                  ft_printf("n deu dog 1"); 
-         }else
-         {
-               if(kill(pid,SIGUSR2)== -1)
-                  ft_printf("n deu dog 0"); 
-         }
-         usleep(200);
-      }
-      i++;
-   }
-   return(i);
+	(void)bacon;
 }
 
-int main (int honc,char ** honey) {
-   // int   client;
+int	main(int honc, char **honey)
+{
+	int	client;
 
-   //  if(honc != 3 || !(100 <= ft_atoi(honey[1]) && ft_atoi(honey[1]) <= 99998))
-   //    return (1);
-   (void)honc;
-   // client = getpid();
-   if(melt(ft_atoi(honey[1]), honey[2]))
-   {
-      ft_printf("po vai sdar smp positivo\n");
-      // melt(client, "\n");
-   }
-   
-   return(0);
+	if (honc != 3)
+		return (1);
+	(void)honc;
+	client = getpid();
+	signal(SIGUSR1, chessburger);
+	ft_melt(ft_atoi(honey[1]), ft_itoa(client), 100);
+	ft_melt(ft_atoi(honey[1]), honey[2], 1000);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedro-o <jpedro-o@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 18:01:20 by jpedro-o          #+#    #+#             */
+/*   Updated: 2023/02/10 16:17:01 by jpedro-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mini_talk.h"
 
 char	*ft_cleaner(char *value)
@@ -10,37 +22,56 @@ char	*ft_cleaner(char *value)
 	return (NULL);
 }
 
-void cheddar(int bit){
+int	bacon(int eggs, char *pizza)
+{
+	int	pid;
+
+	pid = eggs;
+	if (!pid)
+		pid = ft_atoi(pizza);
+	else
+	{
+		ft_printf("%s", pizza);
+		pid = 0;
+	}
+	return (pid);
+}
+
+void	cheddar(int bit)
+{
 	static int	power;
 	static int	letter;
 	static char	*pizza;
+	static int	pid;
 
 	if (bit == SIGUSR1)
 		letter += 1 << (7 - power);
 	power++;
+	if (pid)
+		kill(pid, SIGUSR1);
 	if (power == 8)
 	{
 		if (letter)
-			pizza = ft_joinl(pizza,letter);
+			pizza = ft_joinl(pizza, letter);
 		else
-			{
-				ft_printf("%s",pizza);
-				pizza = ft_cleaner(pizza);
-			}
+		{
+			pid = bacon(pid, pizza);
+			pizza = ft_cleaner(pizza);
+		}
 		power = 0;
 		letter = 0;
 	}
 }
 
-int main () {
-   int	pid;
+int	main(void)
+{
+	int	pid;
 
 	pid = getpid();
-	ft_printf("%d",pid);
+	ft_printf("%d", pid);
 	signal(SIGUSR1, cheddar);
 	signal(SIGUSR2, cheddar);
 	while (1)
 		pause();
-	
-   return(0);
+	return (0);
 }
